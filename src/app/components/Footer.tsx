9aae74b1458ea9c147ca796459df20293168f160
Leul Mesfin <leulmesfin152025@gmail.com>
2024-10-21 05:45:09 -0400

    basic page ui done

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const updateDate = (): number => {
+    const date = new Date();
+    return date.getFullYear();
+}
+const Footer = () => {
+  return (
+    <div className='flex flex-row justify-center gap-4 bg-black items-center h-14 text-white'>
+        &copy; {updateDate()} | Hiwot Studio
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Image from 'next/image';
+
+const Navbar = () => {
+  return (
+    <div className='flex flex-row bg-black'>
+        <div className=''>
+          <Image src='/hs-image.png' width='100' height='100' alt='logo'/>
+        </div>
+        
+        <div className='cursor-pointer flex flex-row gap-4 items-center'>
+          <h1>Services</h1>
+          <h1>Gallery</h1>
+          <h1>About</h1>
+          <h1>Shop</h1>
+          <h1>Book Now</h1>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Navbar
