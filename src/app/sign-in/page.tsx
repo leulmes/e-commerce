51923248d15c362a35a9302ba1dc48dcdbafaf72
Leul Mesfin <leulmesfin152025@gmail.com>
2024-11-14 15:18:27 -0500

    google sign in btn complete

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { FcGoogle } from "react-icons/fc";
+
 import {
 	Card,
 	CardContent,
@@ -13,16 +17,27 @@ const SignIn = () => {
 			<Card className="w-[500px] h-[500px] shadow-xl">
 				<CardHeader className="flex items-center justify-center">
 					<CardTitle>Sign in to VintageScholar</CardTitle>
-					<CardDescription>Your 1 stop shop for vintage college clothing!</CardDescription>
+					<CardDescription>
+						Your 1 stop shop for vintage college clothing!
+					</CardDescription>
 				</CardHeader>
 				<CardContent className="flex items-center justify-center">
-					<button className="bg-black text-white"> Google </button>
+					<button className="flex gap-3 items-center justify-center border rounded-lg py-1 px-4 shadow-sm w-[70%] hover:bg-gray-100">
+						<FcGoogle className="text-lg" />
+						<p className="text-sm text-gray-600 font-medium">
+							Continue with Google
+						</p>
+					</button>
 				</CardContent>
+				<div className="relative flex py-5 items-center justify-center ">
+					<div className="flex-grow border-t border-gray-400"></div>
+					<span className="flex-shrink mx-4 text-gray-400">or</span>
+					<div className="flex-grow border-t border-gray-400"></div>
+				</div>
 				<CardFooter className="flex items-center justify-center">
 					<p>Don't have an account? Sign Up</p>
 				</CardFooter>
 			</Card>
-			
 		</div>
 	);
 };
