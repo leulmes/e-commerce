c73d5849fa4e5fc52d3dd196d687323163edb071
Leul Mesfin <leulmesfin152025@gmail.com>
2024-11-18 16:58:34 -0500

    updated webhook secret name

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -3,14 +3,14 @@ import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
 
 export async function POST(req: Request) {
-  const SIGNING_SECRET = process.env.SIGNING_SECRET
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
 
-  if (!SIGNING_SECRET) {
-    throw new Error('Error: Please add SIGNING_SECRET from Clerk Dashboard to .env or .env.local')
+  if (!WEBHOOK_SECRET) {
+    throw new Error('Error: Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
   }
 
   // Create new Svix instance with secret
-  const wh = new Webhook(SIGNING_SECRET)
+  const wh = new Webhook(WEBHOOK_SECRET)
 
   // Get headers
   const headerPayload = headers()
